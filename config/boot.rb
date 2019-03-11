4a8a761cf24f146821f1f392a0a9f8347c061d4f
Curtis Ovard <ovardcj@gmail.com>
2019-03-11 15:15:37 -0600

    sync the ruby stdout to work with docker

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
+$stdout.sync = true # switch off ruby's output buffering causes issues with docker-compose
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
